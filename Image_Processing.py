3c22ff66fdc58aeb29478443b36a0a1820bdb90f
Felipe Pinto <linkrag.felipe@gmail.com>
2020-10-20 01:17:27 -0300

    Update Image_Processing.py
    Changed the neural network size.

--- Image_Processing.py
+++ Image_Processing.py
@@ -48,24 +48,9 @@ normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
 image_batch, labels_batch = next(iter(normalized_ds))
 first_image = image_batch[0]
 
-num_classes = 12
+num_classes = 10
 
 # Modelo da Rede
-model = Sequential([
-    layers.experimental.preprocessing.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),
-    layers.Conv2D(16, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(32, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Flatten(),
-    layers.Dense(256, activation='relu'),
-    layers.Dense(num_classes),
-    layers.Dense(128, activation='relu'),
-    layers.Dense(num_classes),
-    #layers.Dense(1, activation='sigmoid')
-])
 
 data_augmentation = keras.Sequential(
     [
@@ -80,40 +65,77 @@ data_augmentation = keras.Sequential(
 
 model = Sequential([
     data_augmentation,
+    # Número de canais e tamanho do filtro; sem alterar o tamanho da imagem
     layers.experimental.preprocessing.Rescaling(1. / 255),
     layers.Conv2D(16, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(32, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
-    layers.Conv2D(64, 3, padding='same', activation='relu'),
-    layers.MaxPooling2D(),
+
+    # Elimina algumas conexões entre as camadas para evitar overfitting
     layers.Dropout(0.2),
+
+    # Faz a normalização do batch para começar a próxima camada com a mesma ditribuição
+    layers.BatchNormalization(),
+
+    # Mais uma camada para melhorar o aprendizado da RN
+    layers.Conv2D(32, 3, padding='same', activation='relu'),
+
+    # Faz com que a RN fique mais bobusta para aprender padrões relevantes
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Dropout(0.2),
+    layers.BatchNormalization(),
+
+    # Mais camadas para a RN trabalhar
+    layers.Conv2D(64, 3, padding='same', activation='relu'),
+    layers.MaxPooling2D(pool_size=(2, 2)),
+    layers.Dropout(0.2),
+    layers.BatchNormalization(),
+    layers.Conv2D(128, 3, padding='same', activation='relu'),
+    layers.Dropout(0.2),
+    layers.BatchNormalization(),
+
+    # Faz o nivelamento dos dados
     layers.Flatten(),
+    layers.Dropout(0.2),
+
+    # Camda densa, especificando a quantidade de neuronios, decrescendo até aproximar a quantidade de
+    # classes nesse modelo (100) / a função constraint normaliza os dados evitando o overfitting, por isso ele
+    # vem antes.
+    layers.Dense(512, activation='relu'),
+    layers.Dense(num_classes),
+    layers.Dropout(0.2),
+    layers.BatchNormalization(),
+
     layers.Dense(256, activation='relu'),
     layers.Dense(num_classes),
+    layers.Dropout(0.2),
+    layers.BatchNormalization(),
+
     layers.Dense(128, activation='relu'),
     layers.Dense(num_classes),
-    #layers.Dense(1, activation='sigmoid')
+    layers.Dropout(0.2),
+    layers.BatchNormalization()
+
 ])
 
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
-"""
-#Treino
+# """
+# Treino
 epochs = 50
 history = model.fit(
-  train_ds,
-  validation_data=val_ds,
-  epochs=epochs
+    train_ds,
+    validation_data=val_ds,
+    epochs=epochs
 )
-"""
+# """
+
 # model.summary()
-#""""
-test_image = image.load_img('Teste/Tijolo 01.jpg', target_size=(img_height, img_height))
+
+"""
+test_image = image.load_img('Teste/Martelo.jpg', target_size=(img_height, img_height))
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
-result = model.predict(test_image)
+# result = model.predict(test_image)
 # train_ds.class_indices
 
 predictions = model.predict(test_image)
@@ -123,4 +145,4 @@ print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
         .format(class_names[np.argmax(score)], 100 * np.max(score))
 )
-#"""
+"""
