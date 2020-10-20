1306d86b89a0dd1df450c9c9ce248d6c5be0597c
Felipe Pinto <linkrag.felipe@gmail.com>
2020-10-20 01:21:26 -0300

    Update Image_Searcher.py
    Some code "cleaning" and comments.
    Add the argument "format" for extension search.

--- Image_Searcher.py
+++ Image_Searcher.py
@@ -5,29 +5,19 @@ from PIL import Image
 import cv2
 import os
 
-busca1 = "Tijolo"
-busca2 = "telha"
-busca3 = "concreto"
-busca4 = "cimento"
-busca5 = "brita de construcao"
-busca6 = "prego"
-busca7 = "areia construcao"
-busca8 = "porta"
-busca9 = "tabua"
-busca10 = "vergalhao"
-busca11 = "viga de aco"
-busca12 = "tubos de aco"
-busca13 = "trelica"
+busca = ""
+
 
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"viga construcao", "limit":100, "path":busca1}   #creating list of arguments
+arguments = {"keywords":busca, "limit":100, "path":busca, "format":"jpg", "format":"png"}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded images
 
-IMG_DIR = 'C:/Users/Felipe Pinto/Roaming/Documents/Python/APS_6/downloads/' + busca2
+IMG_DIR = 'C:/Users/Felipe Pinto/Roaming/Documents/Python/APS_6/downloads/' + busca
 
+# Create a csv doc with the images searched 
 for img in os.listdir(IMG_DIR):
     img_array = cv2.imread(os.path.join(IMG_DIR, img), cv2.IMREAD_GRAYSCALE)
 
